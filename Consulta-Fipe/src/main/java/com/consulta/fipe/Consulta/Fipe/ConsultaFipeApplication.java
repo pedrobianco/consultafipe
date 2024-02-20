d29327cbba890eccdfc671e515d7a9b72f92044c
Pedro Bianco <pedroalasticobianco@gmail.com>
2024-02-20 20:48:06 -0300

    feat
    Start project

---
+++ Consulta-Fipe/src/main/java/com/consulta/fipe/Consulta/Fipe/ConsultaFipeApplication.java
@@ -0,0 +1,13 @@
+package com.consulta.fipe.Consulta.Fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFipeApplication.class, args);
+	}
+
+}
---
+++ Consulta-Fipe/src/test/java/com/consulta/fipe/Consulta/Fipe/ConsultaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulta.fipe.Consulta.Fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
